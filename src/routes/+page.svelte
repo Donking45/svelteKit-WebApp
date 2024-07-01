5a7dfa82a81a18e866ad7a417c337aef1cbd25eb
KINGSLEY OKON <kingsleyokon610@gmail.com>
2024-07-01 17:56:22 +0100

    tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="ml-5 pt-5 pl-10 hover:underline font-bold hover:text-purple-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
